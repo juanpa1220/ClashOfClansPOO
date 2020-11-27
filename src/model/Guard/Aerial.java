655b97cac1044ae7374607fd8a913d6c52235f82
Katerine Guzman <guzka97@gmail.com>
2020-11-27 01:50:54 -0600

    set up Guards

--- src/model/Guard/Aerial.java
+++ src/model/Guard/Aerial.java
@@ -5,8 +5,16 @@ import model.BoardItem;
 import java.util.ArrayList;
 
 public class Aerial extends Guard {
-    public Aerial(ArrayList<BoardItem> refBoard, String name, String dirImage, int appearanceLevel, int level, int life, int hits, int housingSpace, int scope, Object objectiveWarrior) {
-        super(refBoard, name, dirImage, appearanceLevel, level, life, hits, housingSpace, scope, objectiveWarrior);
+    public Aerial(ArrayList<BoardItem> refBoard,
+                  String name,
+                  String dirImage,
+                  int appearanceLevel,
+                  int level,
+                  int housingSpace,
+                  Object objectiveWarrior,
+                  String type
+    ) {
+        super(refBoard, name, dirImage, appearanceLevel, level, 1000, 1000, housingSpace, objectiveWarrior,type);
     }
 
 }
--- src/model/Guard/Wall.java
+++ src/model/Guard/Wall.java
@@ -5,7 +5,15 @@ import model.BoardItem;
 import java.util.ArrayList;
 
 public class Wall extends Guard {
-    public Wall(ArrayList<BoardItem> refBoard, String name, String dirImage, int appearanceLevel, int level, int life, int hits, int housingSpace, int scope, Object objectiveWarrior) {
-        super(refBoard, name, dirImage, appearanceLevel, level, life, hits, housingSpace, scope, objectiveWarrior);
+    public Wall(ArrayList<BoardItem> refBoard,
+                  String name,
+                  String dirImage,
+                  int appearanceLevel,
+                  int level,
+                  int housingSpace,
+                  Object objectiveWarrior,
+                  String type
+    ) {
+        super(refBoard, name, dirImage, appearanceLevel, level, 1000,1000, housingSpace, objectiveWarrior,type);
     }
 }
